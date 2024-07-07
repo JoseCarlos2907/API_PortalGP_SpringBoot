cb84d8c93e0849da68337b3fdb75cd7bab9777d9
José Carlos <josecarlosperezgonzalez200@gmail.com>
2024-07-07 18:47:57 +0200

    primer commit de comienzo de proyecto

---
+++ com.app.portalgp/src/main/java/com/app/api/ApiPortalGpApplication.java
@@ -0,0 +1,13 @@
+package com.app.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPortalGpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPortalGpApplication.class, args);
+	}
+
+}
